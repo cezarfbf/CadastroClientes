c5d8dd9aa8baaf674e25f2b27da926a51f34f10a
Cezar Filho <35531208+cezarfbf@users.noreply.github.com>
2018-07-04 00:02:25 -0300

    Annotation Entity em Cliente

--- src/main/java/br/com/deal/CadastroClientes/models/Cliente.java
+++ src/main/java/br/com/deal/CadastroClientes/models/Cliente.java
@@ -1,12 +1,16 @@
 package br.com.deal.CadastroClientes.models;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Cliente {
+public class Cliente implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
@@ -19,6 +23,13 @@ public class Cliente {
 	private Contato contato;
 	
 	
+	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
 	public String getNome() {
 		return nome;
 	}
