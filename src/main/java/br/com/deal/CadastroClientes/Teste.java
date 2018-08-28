487d7b6ad0e9ee580af3f7e9e0d810ca34215cb7
Cezar Filho <35531208+cezarfbf@users.noreply.github.com>
2018-08-28 17:56:14 -0300

    Testes de persistencia

--- src/main/java/br/com/deal/CadastroClientes/Teste.java
+++ src/main/java/br/com/deal/CadastroClientes/Teste.java
@@ -1,12 +1,36 @@
 package br.com.deal.CadastroClientes;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.deal.CadastroClientes.models.Cliente;
+import br.com.deal.CadastroClientes.models.Endereco;
 import br.com.deal.CadastroClientes.persistence.ConnectionMySQL;
+import br.com.deal.CadastroClientes.repository.ClienteRepository;
 
 public class Teste {
+//	@Autowired
+	private static ClienteRepository dao;
 	
 	public static void main(String arg[]) {
 		
-		ConnectionMySQL.getConnection();
+//		Connection conn = ConnectionMySQL.getConnection();
+		
+		Cliente cli = new Cliente("Teste", "1098908709832081", "PE");
+		cli.setEmails(new ArrayList<>());
+		cli.setEndereco(new Endereco());
+		cli.setTelefones(new ArrayList<>());
+		cli.setResponsaveis(new ArrayList<>());
+		cli.setCodgio(10);
+		
+		try {
+        dao.save(cli);
+		}catch (Exception e) {
+			System.out.println(e);
+		}
+		
 		
 	}
 
